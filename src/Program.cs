26d98493e416dbec0ea54b40692da3a577d77d1f
Rickas Razafison <v-riraza@microsoft.com>
2017-05-31 16:29:01 +0800

    added basic html generator based on dotnet-core and Razor template (Razorlight lib)

---
+++ src/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using RazorLight;
+using RazorLight.Extensions;
+namespace PersoBrandStaticGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 set base directory of template folder
+            var fileInfo = new FileInfo("../content/template");
+            var engine = EngineFactory.CreatePhysical(fileInfo.FullName);
+
+            //2 generate html string based on template file and model
+            string indexHtml = engine.Parse("index.cshtml", "");
+
+            //3 save to output folder
+            string outputFolder = Path.Combine(AppContext.BaseDirectory, "dist");
+            if (!Directory.Exists(outputFolder))
+                Directory.CreateDirectory(outputFolder);
+            File.WriteAllText(Path.Combine(outputFolder, "index.html"), indexHtml);
+
+        }
+    }
+}
