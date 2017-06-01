93b5196ae16622a7cead4e4ca555144ffb375923
Rickas Razafison <v-riraza@microsoft.com>
2017-06-01 20:06:29 +0800

    added watch feature and live reload with browser-sync in dev mode

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var del = require('del');
 var changed = require('gulp-changed');
 var sourceContentPath = "content";
 var destContentPath = "dist";
+var browserSync = require('browser-sync');
+var exec = require('child_process').exec;
+var gutil = require('gulp-util');
+
 ///
 //
 // Clean Section
@@ -39,4 +43,47 @@ gulp.task('copy', function () {
         ])
         .pipe(changed(destContentPath))
         .pipe(gulp.dest(destContentPath));
+});
+
+
+///
+//
+// Watch Section 
+//
+///
+
+/**
+ * Watch all
+ */
+gulp.task('watch', function () {
+    //watch any file under /dist
+    browserSync.init({
+        server: "dist",
+        port: 3470, //specify the port to be used by browserify 
+        files: [ //specify the files to watch (use regex)
+            './dist/**/*.*'
+        ],
+        injectChanges: true,
+        logFileChanges: true,
+        logLevel: 'debug',
+        logPrefix: 'browser-sync', //name to display in console
+        notify: true,
+        reloadDelay: 100 //the delay to wait before reloading a file
+    });
+
+    //watch css, images and static js
+    gulp.watch([sourceContentPath + '/**/*.css',
+        sourceContentPath + '/**/*.js',
+        sourceContentPath + '/**/*.jpg',
+        sourceContentPath + '/**/*.gif'
+    ], ['copy']);
+
+    //watch cshtml razor templates and md files
+    gulp.watch([
+        sourceContentPath + '/**/*.cshtml',
+        sourceContentPath + '/**/*.md'
+    ], function () {
+        gutil.log('cshtml/md files updated, rebuilding...');
+        exec('npm run build-pages');
+    });
 });
