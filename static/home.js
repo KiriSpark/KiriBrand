f6ba10bbb2c49e4cd1ee51dcf9253d2633be12e2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-06-16 11:35:20 +0000

    Deploy KiriSpark/KiriBrand to github.com/KiriSpark/KiriBrand.git:gh-pages

---
+++ static/home.js
@@ -0,0 +1,73 @@
+$(window).load(function () {
+    //set a default timout for the loader animation
+    //so people can see it
+    setTimeout(function () {
+        //hide the loader
+        $(".se-pre-con").hide();
+        //call page animation
+        //animateHeader();
+        animateSections();
+        animateLandingScrollButton();
+        $('#landing_actionlist').stickUp({
+            topMargin: '0px'
+        });
+    }, 560);
+});
+//function to animate the header buttons
+function animateHeader() {
+    var delay = 100;
+    $('.nav-item').each(function (index, item) {
+        animateNavButton('#' + item.id, delay);
+        delay += 150;
+    });
+}
+//function to animate one particular button by its Id name
+//using Velocity library
+function animateNavButton(itemName, delay) {
+    if (delay === void 0) {
+        delay = 0;
+    }
+    $(itemName).velocity('transition.flipXIn', {
+        delay: delay,
+        opacity: 1,
+        duration: 1000
+    });
+}
+//this will animate sections inside the body
+//using scrolReveal library
+function animateSections() {
+    var config = {
+        duration: 300,
+        delay: 600,
+        distance: '200px',
+        origin: 'bottom',
+        easing: 'cubic-bezier(1, 1, 1, 1)'
+    };
+    var sr = window.sr = ScrollReveal();
+    $('.animated').each(function (index, item) {
+        sr.reveal(item, config);
+    });
+}
+
+function animateLandingScrollButton() {
+    $('.smoothscroll > i').velocity({
+        "margin-top": "8px"
+    }, {
+        loop: true
+    });
+    $('#scrolldown').on('click', function (e) {
+        e.preventDefault();
+
+        //using the navbar height as offset because the navbar
+        //will be stuck on the top when we scroll, so we need to consider
+        //its size in the landing position
+        var scrollBarHeight = $('#landing_actionlist').height();
+        $('#body_area').velocity("scroll", {
+            offset: -(scrollBarHeight) + 5,
+            duration: 800,
+            easing: "swing"
+        })    .velocity({
+            opacity: 1
+        });
+    });
+}
